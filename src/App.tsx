334095d2af4ad05a18ada61739623ad8e158157c
Renan Vicente <renanvicentecosta@gmail.com>
2023-06-27 16:09:39 -0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const email = event.currentTarget.email.value
+    const password = event.currentTarget.password.value
+    if(email === "renan@exemplo.com" && password === "123456") {
+      alert("Bem vindo, Renan!")
+    } else {
+      alert("Email ou senha incorretos!")
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Acessar minha conta</h2>
+      <form onSubmit={handleLogin} className='form-container'>
+        <input name='email' placeholder='Email' className='input' type='email' />
+        <input name='password' placeholder='Senha' className='input' type='password' />
+        <button className='button' type="submit">Entrar</button>
+      </form>
     </div>
   );
 }
